b991d4e64ae6e1d5e301a72aa2fff5ffb89568db
Thomas <thomasjohnso@gmail.com>
2023-02-10 11:58:40 -0500

    found most frequent word for 2nd and 3rd poems and found password. words are 'happy' 'you' 'love' password is 'love'

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-const { readFile } = require('fs').promises;
+const { readFile } = require("fs").promises;
 
 const mostFrequentWord = (text) => {
   const words = text.toLowerCase().match(/[^_\W]+/g);
   const tally = {};
   let mostFrequentWord = null;
-  
-  words.forEach(word => {
-    tally[word] = (tally[word] || 0) + 1 ;
-    if(!tally[mostFrequentWord] || tally[word] > tally[mostFrequentWord])
+
+  words.forEach((word) => {
+    tally[word] = (tally[word] || 0) + 1;
+    if (!tally[mostFrequentWord] || tally[word] > tally[mostFrequentWord])
       mostFrequentWord = word;
   });
   return mostFrequentWord;
-}
+};
 
 const findPassword = async () => {
   // Your code goes here
-  let firstPoem = await readFile('poems/starting-poem.txt', 'utf-8');
+  let firstPoem = await readFile("poems/starting-poem.txt", "utf-8");
   let firstPoemWord = mostFrequentWord(firstPoem);
   console.log(firstPoemWord);
-}
+  let secondPoem = await readFile("poems/happy.txt", "utf-8");
+  let secondPoemWord = mostFrequentWord(secondPoem);
+  console.log(secondPoemWord);
+  let thirdPoem = await readFile("poems/you.txt", "utf-8");
+  let thirdPoemWord = mostFrequentWord(thirdPoem);
+  console.log(thirdPoemWord);
+};
 
 findPassword();
